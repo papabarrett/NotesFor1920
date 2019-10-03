e2f82780ba26e9f6d2f8d045e1c3e2a211e9d1fd
papabarrett <55889600+papabarrett@users.noreply.github.com>
2019-10-03 14:54:19 -0500

    Notes on Loops

--- ProgNotes2019SeptOct/src/CitySkyline.java
+++ ProgNotes2019SeptOct/src/CitySkyline.java
@@ -1,3 +1,4 @@
+
 /**
  *
  * @author NAME
@@ -19,18 +20,16 @@ public class CitySkyline implements ActionListener, KeyListener {
 
     public CitySkyline() {
         frame = new JFrame("Insert Title Here");
-        frame.setSize(500, 500);
+        frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        frame.setUndecorated(true);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         display = new DisplayPanel();
         frame.add(display);
         //put constructor code here
-        
-        
-        
-        
+
         //end your constructor code
         timer = new javax.swing.Timer(10, this);
-        timer.start();
+        // timer.start();
         frame.addKeyListener(this);
         frame.setFocusable(true);
         frame.setVisible(true);
@@ -38,8 +37,7 @@ public class CitySkyline implements ActionListener, KeyListener {
 
     public void actionPerformed(ActionEvent e) {
         //type what needs to be performed every time the timer ticks
-        
-        
+System.out.println("call1");
         //end your code for timer tick code
         display.repaint();
     }
@@ -51,21 +49,41 @@ public class CitySkyline implements ActionListener, KeyListener {
     }
 
     public void keyReleased(KeyEvent e) {
+        System.exit(0);
     }
 
     class DisplayPanel extends JPanel {
 
+        
+        final int STREET_LEVEL = 730;
+        final int STREET_WIDTH = 20;
+
         public void paintComponent(Graphics g) {
             super.paintComponent(g);
             //draw your graphics here
-            setBackground(new Color(24,21,62));
+            int x = 50;
+            setBackground(new Color(24, 21, 62));
             g.setColor(Color.white);
-            g.fillOval(10,10,20,20);
-            g.setColor(new Color(138,215,200,150));
-            g.fillOval(0,0,100,30);
-            g.fillOval(30, 10, 100, 20);
+            g.fillOval(10, 10, 20, 20);
+            g.setColor(Color.LIGHT_GRAY);
+            g.fillRect(0, STREET_LEVEL, 5000, STREET_WIDTH);
             
-            
+            while(x<400)
+                x=drawBuilding(g,x);
+            g.setColor(Color.red);
+            g.drawRect(0, 0, 800, 800);
+        }
+
+        public int drawBuilding(Graphics g, int x) {
+            int width = (int) (Math.random() * 101 + 50);//from 50 to 150
+            int height = (int) (Math.random() * STREET_LEVEL * .5 + STREET_LEVEL * .25);
+            Color buidling = new Color((int) (Math.random() * 255),
+                    (int) (Math.random() * 255), (int) (Math.random() * 255));
+            g.setColor(buidling);
+            int yBuilding = STREET_LEVEL - height;
+            g.fillRect(x, yBuilding, width, height);
+            x += width;
+            return x;
         }
     }
 }
