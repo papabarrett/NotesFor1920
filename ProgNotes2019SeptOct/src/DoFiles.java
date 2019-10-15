faf2e59d6949788cdb1a85c08d0691b91356106b
papabarrett <55889600+papabarrett@users.noreply.github.com>
2019-10-15 10:40:53 -0500

    Do while loop lesson

---
+++ ProgNotes2019SeptOct/src/DoFiles.java
@@ -0,0 +1,51 @@
+
+/**
+ *
+ * @author J. Barrett
+ */
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+import java.text.DecimalFormat;
+
+public class DoFiles {
+
+    public static void main(String[] args) throws Exception {
+        File f = new File("input.txt");
+        f.createNewFile();
+
+        Scanner scan = new Scanner(f);
+        
+        do{
+        processText(scan);
+        }while(scan.hasNext());
+
+    }
+
+    public static void processText(Scanner scan) {
+        String shortWord, longWord, currentWord;
+        double wordCount = 0, letterCount = 0;
+        shortWord = longWord = currentWord = scan.next();
+        do {
+            currentWord = scan.next();
+            if (!currentWord.startsWith("-")) {
+                // System.out.println("+++Scanning " + currentWord);
+                wordCount++;
+                letterCount += currentWord.length();
+                if (currentWord.length() > longWord.length()) {
+                    longWord = currentWord;
+                }
+                if (currentWord.length() < shortWord.length()) {
+                    shortWord = currentWord;
+                }
+            }
+        } while (!currentWord.equals("---"));
+        //do-while is only loop with semi-colon
+        System.out.println("The longest word is " + longWord);
+        System.out.println("The shortest word is " + shortWord);
+        System.out.println("The average word length is "+
+                new DecimalFormat("0.0").format((letterCount/wordCount)));
+        System.out.println("-----------------------------------");
+    }
+
+}
