4a5ed5de1631370f8c70bb474aef33fc512dc040
papabarrett <55889600+papabarrett@users.noreply.github.com>
2019-10-31 10:47:06 -0500

    ArraysLesson

--- ProgNotes2019SeptOct/src/MapDisplay.java
+++ ProgNotes2019SeptOct/src/MapDisplay.java
@@ -15,6 +15,8 @@ public class MapDisplay implements ActionListener, KeyListener {
     JFrame frame;
     JPanel display;
     int tileWidth=1, tileHeight=1;
+    
+    Rectangle[] monsters;
 
     public static void main(String[] args) throws Exception {
         new MapDisplay();
@@ -28,13 +30,47 @@ public class MapDisplay implements ActionListener, KeyListener {
         display = new DisplayPanel();
         frame.add(display);
         //put constructor code here
-        
+        int mon=0;
+        try {
+            Scanner scan=new Scanner(new File("map.txt"));
+                int rows=0, cols=0;
+                while(scan.hasNext()){
+                    String line=scan.next();
+                    cols=line.length();
+                    mon+=cols-line.replaceAll("m","").length();
+                    rows++;
+                }
+                System.out.println("t");
+                scan=new Scanner(new File("map.txt"));
+                monsters=new Rectangle[mon];    
+                mon=0;
+                System.out.println("t");
+                tileHeight=Toolkit.getDefaultToolkit().getScreenSize().height/rows+1;
+                System.out.println(tileHeight);
+                rows=0;
+                tileWidth=Toolkit.getDefaultToolkit().getScreenSize().width/cols+1;
+                System.out.println("y");
+                while(scan.hasNext()){
+                    String line=scan.next();
+                    for (int i = 0; i < line.length(); i++) {
+                        if(line.charAt(i)=='m'){
+                            monsters[mon]=new Rectangle(i*tileWidth, rows*tileHeight,
+                                        tileWidth,tileHeight);
+                            mon++;
+                            System.out.println(monsters[mon-1]+" added");
+                        }
+                    }
+                    rows++;
+                }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
         
         
         
         //end your constructor code
         timer = new javax.swing.Timer(10, this);
-      //  timer.start();
+        timer.start();
         frame.addKeyListener(this);
         frame.setFocusable(true);
         frame.setVisible(true);
@@ -42,7 +78,18 @@ public class MapDisplay implements ActionListener, KeyListener {
 
     public void actionPerformed(ActionEvent e) {
         //type what needs to be performed every time the timer ticks
-        
+        int selected=(int)(Math.random()*monsters.length);
+        if(Math.random()<.5){ 
+                monsters[selected].y+=8;
+        }
+        else{               
+                monsters[selected].x+=5;
+                        
+        }
+        if(display.getHeight()>100)
+        if(monsters[selected].y>display.getHeight()) monsters[selected].y=0;
+        if(display.getHeight()>100)
+        if(monsters[selected].x>display.getWidth()) monsters[selected].x=0;
         
         //end your code for timer tick code
         display.repaint();
@@ -93,6 +140,12 @@ public class MapDisplay implements ActionListener, KeyListener {
                         }
                     }
                     r++;
+                }
+                for (int i = 0; i < monsters.length; i++) {
+                    Rectangle monster = monsters[i];
+                    g.setColor(Color.red);
+                    g.fillOval(monster.x, monster.y, monster.width, monster.height);
+                    
                 }
             } catch (Exception e) {
             }
